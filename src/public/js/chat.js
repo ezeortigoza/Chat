70fc4f6b3cc293de7aaf5ac6ea392c25128deab9
ezeortigoza <eortigozaportillo@gmail.com>
2022-07-28 11:31:59 -0300

    nuevo commit

---
+++ src/public/js/chat.js
@@ -0,0 +1,50 @@
+let username;
+const chatBox = document.getElementById('chatBox');
+const socket = io({
+    autoConnect:false
+});
+
+Swal.fire({
+    title:"Identifícate",
+    input:"text",
+    text:"Ingresa el usuario con el que te identificarás en el chat",
+    inputValidator: (value) =>{
+        return !value && "Necesitas identificarte para poder continuar >:("
+    },
+    allowOutsideClick:false,
+    allowEscapeKey:false
+}).then(result=>{
+    username = result.value;
+    socket.connect();
+})
+
+/*Listeners */
+chatBox.addEventListener('keyup',evt=>{
+    if(evt.key==="Enter"){
+        if(chatBox.value.trim().length>0){
+            socket.emit('message',{user:username,message:chatBox.value})
+            chatBox.value="";
+        }
+    }
+})
+
+/*Listeners */
+
+socket.on('log',data=>{
+    let log = document.getElementById('log');
+    let messages = "";
+    data.forEach(message=>{
+        messages = messages+`${message.user} dice: ${message.message}</br>`
+    })
+    log.innerHTML = messages;
+})
+socket.on('newUser', data =>{
+    if(username){
+        Swal.fire({
+            text:"Nuevo usuario en el chat",
+            toast:true,
+            postion:"top-right",
+            timer: 2000
+        })
+    }
+})
